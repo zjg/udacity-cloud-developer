ff9fa8f5ee78cc2f8384c214bad18d28f0068bc2
Doug Rosvick <drosvick@gmail.com>
2021-08-03 00:52:11 -0600

    Fix deprecated usage of body-parser

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -13,7 +13,7 @@ import { Car, cars as cars_list } from './cars';
   
   //use middleware so post bodies 
   //are accessable as req.body.{{variable}}
-  app.use(bodyParser.json()); 
+  app.use(express.json()); 
 
   // Root URI call
   app.get( "/", ( req: Request, res: Response ) => {
