1cfa0206b320cc1f06f54ac9bb10161050748746
Doug Rosvick <drosvick@gmail.com>
2021-10-31 00:17:22 -0600

    Update appId and switch frontend to deployed app

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,7 +1,6 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-// export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
-export const apiEndpoint = 'http://localhost:4000/dev'
+const apiId = '9q9a0h7a61'
+export const apiEndpoint = `https://${apiId}.execute-api.us-west-1.amazonaws.com/dev`
+// export const apiEndpoint = 'http://localhost:4000/dev'
 
 export const authConfig = {
   domain: 'dev-t3dolc50.us.auth0.com',            // Auth0 domain
