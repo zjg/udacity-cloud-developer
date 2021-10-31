8ef31a4ef52a1011d7b5d27dc7f118e64f904842
Doug Rosvick <drosvick@gmail.com>
2021-10-30 22:41:04 -0600

    client URL for dev

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,6 +1,7 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+// export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+export const apiEndpoint = 'http://localhost:4000/dev'
 
 export const authConfig = {
   domain: 'dev-t3dolc50.us.auth0.com',            // Auth0 domain
